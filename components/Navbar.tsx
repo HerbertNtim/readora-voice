e9a2de48030d0861811a2c719ebc82b7dcefc419
HerbertNtim <herbertntim15@gmail.com>
2026-04-13 11:57:34 +0000

    implemented: logo

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,11 +1,13 @@
+import Image from 'next/image';
 import Link from 'next/link';
 
 const Navbar = () => {
   return (
     <header className="w-full fixed z-50 bg-('bg-primary')">
       <div className="flex navbar-height">
-        <Link href="/" className="">
-          Readora
+        <Link href="/" className="flex items-center gap-0.5">
+          <Image src="/logo.png" alt="Readora Logo" width={42} height={26} />
+          <span className="logo-text">Readora</span>
         </Link>
       </div>
     </header>
