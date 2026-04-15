37307e7edf082452447b4e5a6b05e613e8fad87c
HerbertNtim <herbertntim15@gmail.com>
2026-04-15 07:03:05 +0000

    implementing: add new book page

---
+++ app/(root)/books/new/page.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const AddBook = () => {
+  return (
+    <main className="wrapper container">
+      <div className="mx-auto max-w-180 space-y-10">
+        <section className="flex flex-col gap-5">
+          <h1 className="page-title-xl">Add New Book</h1>
+          <p className="subtitle">Add a new book to interact with the book.</p>
+        </section>
+      </div>
+    </main>
+  );
+};
+
+export default AddBook;
