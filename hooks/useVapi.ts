e3b1cc83fed6064ba9e158cc2ab183ded2ee4433
HerbertNtim <herbertntim15@gmail.com>
2026-05-05 16:54:31 +0000

    use vapi updates

--- hooks/useVapi.ts
+++ hooks/useVapi.ts
@@ -1,6 +1,9 @@
-import { DEFAULT_VOICE } from '@/lib/constants';
+import { startVoiceSession } from '@/lib/actions/session.action';
+import { ASSISTANT_ID, DEFAULT_VOICE, VOICE_SETTINGS } from '@/lib/constants';
+import { getVoice } from '@/lib/utils';
 import { IBook, Messages } from '@/types';
 import { useAuth } from '@clerk/nextjs';
+import Vapi from '@vapi-ai/web';
 import { useEffect, useRef, useState } from 'react';
 
 export type CallStatus =
@@ -20,6 +23,24 @@ const useLatestRef = <T>(value: T) => {
 
   return ref;
 };
+
+const VAPI_API_KEY = process.env.NEXT_PUBLIC_VAPI_API_KEY;
+
+let vapi: InstanceType<typeof Vapi>;
+
+function getVapi() {
+  if (!vapi) {
+    if (!VAPI_API_KEY) {
+      throw new Error(
+        'NEXT_PUBLIC_VAPI_API_KEY not found. Update your env file.',
+      );
+    }
+
+    vapi = new Vapi(VAPI_API_KEY);
+  }
+
+  return vapi;
+}
 
 const useVapi = (book: IBook) => {
   const { userId } = useAuth();
@@ -58,6 +79,37 @@ const useVapi = (book: IBook) => {
     setStatus('connecting');
 
     try {
+      const startSession = await startVoiceSession(userId, book._id);
+
+      if (!startSession.success) {
+        setLimitError(
+          startSession.error || 'Session limit reached. Upgrade your plan!',
+        );
+        setStatus('idle');
+        return;
+      }
+
+      sessionIdRef.current = startSession.sessionId || null;
+
+      const firstMessage = `Hey, good to meet you. Before we dive in: have you read ${book.title} yet? Or are we starting fresh?`;
+
+      await getVapi().start(ASSISTANT_ID, {
+        firstMessage,
+        variableValues: {
+          title: book.title,
+          author: book.author,
+          bookId: book._id,
+        },
+        // voice: {
+        //   provider: '11labs',
+        //   voiceId: getVoice(voice).id,
+        //   model: 'eleven_turbo_v2_5' as const,
+        //   stability: VOICE_SETTINGS.stability,
+        //   similarityBoost:  VOICE_SETTINGS.similarityBoost,
+        //   style: VOICE_SETTINGS.style,
+        //   useSpeakerBoost: VOICE_SETTINGS.useSpeakerBoost
+        // }
+      });
     } catch (error) {
       console.error('Error starting call.', error);
       setStatus('idle');
