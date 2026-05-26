5311f2e5dba07403c5451538b9a60d2633d3162e
HerbertNtim <herbertntim15@gmail.com>
2026-05-26 16:52:28 +0000

    updated the add book page title

--- app/(root)/books/new/page.tsx
+++ app/(root)/books/new/page.tsx
@@ -3,7 +3,7 @@ import UploadForm from '@/components/UploadForm';
 const AddBook = () => {
   return (
     <main className="new-book">
-      <section className="flex flex-col gap-5 text-center">
+      <section className="flex flex-col gap-5 text-center pt-7">
         <h1 className="page-title-xl">Add a New Book</h1>
         <p className="subtitle">
           Upload a PDF to generate your interactive reading experience
